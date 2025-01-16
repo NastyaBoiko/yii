cf1ab99a50232100997f5643585e65f7611090a9
Nastya <n.mikhailova17@mail.ru>
2025-01-16 15:34:54 +0300

    modal

--- modules/admin/views/order/index.php
+++ modules/admin/views/order/index.php
@@ -3,11 +3,14 @@
 use app\models\Order;
 use app\models\Outpost;
 use app\models\Status;
+use app\widgets\Alert;
 use yii\bootstrap5\LinkPager;
+use yii\bootstrap5\Modal;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
+use yii\web\JqueryAsset;
 use yii\widgets\Pjax;
 /** @var yii\web\View $this */
 /** @var app\modules\admin\models\OrderSearch $searchModel */
@@ -30,8 +33,18 @@ $statusesTitle = array_flip($statuses);
         <h3>Управление заказами</h3>
     </div>
     <?php Pjax::begin([
+            'id' => 'admin-order-pjax',
             'enablePushState' => false,
+            'timeout' => 5000
         ]); ?>
+
+        <?php if (Yii::$app->session->hasFlash('cancel-modal-info')) {
+            Yii::$app->session->setFlash('warning', Yii::$app->session->getFlash('cancel-modal-info'));
+            Yii::$app->session->removeFlash('cancel-modal-info');
+            echo Alert::widget();
+        }
+        ?>
+
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
@@ -102,11 +115,13 @@ $statusesTitle = array_flip($statuses);
                 'value' => function($model) use ($statusesTitle) {
                     $view = Html::a('Просмотр', ['view', 'id' => $model->id], ['class' => 'btn btn-outline-primary']);
                     $cancel = '';
+                    $cancel_modal = '';
                     $apply = '';
 
                     if ($model->status_id == $statusesTitle['Новый']) {
                         $apply = Html::a('Подтвердить', ['apply', 'id' => $model->id], ['class' => 'btn btn-outline-success',]);
                         $cancel = Html::a('Отменить', ['cancel', 'id' => $model->id], ['class' => 'btn btn-outline-warning',]);
+                        $cancel_modal = Html::a('Отменить (модалка)', ['cancel-modal', 'id' => $model->id], ['class' => 'btn btn-outline-warning btn-cancel-modal',]);
                                 //     'data' => [
                                 //         'confirm' => 'Точно ли хотите удалить?',
                                 //         'method' => 'post',
@@ -122,7 +137,7 @@ $statusesTitle = array_flip($statuses);
                     //         ],
                     //     ])
                     //     : '';
-                    return "<div class='d-flex gap-3'>$view $apply $cancel</div>";
+                    return "<div class='d-flex flex-wrap gap-3'>$view $apply $cancel $cancel_modal</div>";
                 }
             ],
         ],
@@ -131,3 +146,21 @@ $statusesTitle = array_flip($statuses);
     <?php Pjax::end(); ?>
 
 </div>
+
+<?php
+    if ($dataProvider->count) {
+        Modal::begin([
+            'id' => 'cancel-modal',
+            'title' => 'Отмена заказа',
+            'size' => 'modal-lg',
+        ]);
+
+        echo $this->render('_form-modal', [
+            'model_cancel' => $model_cancel,
+        ]);
+
+        Modal::end();
+
+        $this->registerJsFile('/js/cancel-modal.js', ['depends' => JqueryAsset::class]);
+    }
+?>
